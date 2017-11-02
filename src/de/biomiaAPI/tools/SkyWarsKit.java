524790ac482ef5d123dba66e900d05ba171eab56
Julsn <julian.bildner@biomia.de>
2017-11-02 16:06:26 +0100

    Exteral SW Kit class to add / get / remove Kits

---
+++ src/de/biomiaAPI/tools/SkyWarsKit.java
@@ -0,0 +1,53 @@
+package de.biomiaAPI.tools;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+
+import de.biomiaAPI.BiomiaPlayer;
+import de.biomiaAPI.mysql.MySQL;
+
+public class SkyWarsKit {
+
+	public static void addKit(BiomiaPlayer biomiaPlayer, int kitID, int... months) {
+		MySQL.executeUpdate("Insert into SkyWarsKits (`BiomiaPlayer`, `kitID`, `available`) values ("
+				+ biomiaPlayer.getBiomiaPlayerID() + ", " + kitID + ", '" + months.toString() + "')");
+	}
+
+	public static void addKit(BiomiaPlayer biomiaPlayer, int kitID) {
+		MySQL.executeUpdate("Insert into SkyWarsKits (`BiomiaPlayer`, `kitID`) values ("
+				+ biomiaPlayer.getBiomiaPlayerID() + ", " + kitID + ")");
+	}
+
+	public static void removeKit(BiomiaPlayer biomiaPlayer, int kitID) {
+		MySQL.executeUpdate("Delete from SkyWarsKits where BiomiaPlayer = " + biomiaPlayer.getBiomiaPlayerID()
+				+ " and kitID = " + kitID);
+	}
+
+	public static ArrayList<Integer> getAvailableKit(BiomiaPlayer biomiaPlayer) {
+		ArrayList<Integer> availableKits = new ArrayList<>();
+		Connection con = MySQL.Connect();
+		try {
+			PreparedStatement ps = con.prepareStatement("Select kitID, available from SkyWarsKits where BiomiaPlayer = ?");
+			ps.setInt(1, biomiaPlayer.getBiomiaPlayerID());
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				String available = rs.getString("available");				
+				Calendar c = Calendar.getInstance();
+				
+				if (available == null || Arrays.asList(available.substring(1, available.length() - 1).split(", ")).contains(c.get(Calendar.MONTH) + "")) {
+					availableKits.add(rs.getInt("kitID"));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return availableKits;
+
+	}
+}
