af1eccf146f8cb798f7b1e0ab877b39a4958eae5
Aaron <julian.bildner@biomia.de>
2018-02-14 15:26:26 +0100

    nur für den fall

--- src/de/biomiaAPI/main/Main.java
+++ src/de/biomiaAPI/main/Main.java
@@ -51,6 +51,7 @@ public class Main extends JavaPlugin {
         setUniversalTimoapi(TimoCloudAPI.getUniversalInstance());
         groupName = bukkitTimoapi.getThisServer().getGroupName();
 
+        
         Bukkit.getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
         Bukkit.getMessenger().registerIncomingPluginChannel(this, "BungeeCord", new Connect());
         Bukkit.getMessenger().registerOutgoingPluginChannel(this, "RedisBungee");
