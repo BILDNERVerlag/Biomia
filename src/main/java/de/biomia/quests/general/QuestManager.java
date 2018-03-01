3164ce5adefb32a6004f51927c4b5d02974c20e7
Aaron <julian.bildner@biomia.de>
2018-03-01 10:55:27 +0100

    Added getQuests() method in QuestManager

--- src/main/java/de/biomia/quests/general/QuestManager.java
+++ src/main/java/de/biomia/quests/general/QuestManager.java
@@ -4,33 +4,36 @@ import java.util.ArrayList;
 
 public class QuestManager {
 
-	ArrayList<Quest> quests = new ArrayList<>();
+    ArrayList<Quest> quests = new ArrayList<>();
 
-	public Quest getQuest(int questID) {
-		for (Quest q : quests) {
-			if (q.getQuestID() == questID) {
-				return q;
-			}
-		}
-		return null;
-	}
+    public Quest getQuest(int questID) {
+        for (Quest q : quests) {
+            if (q.getQuestID() == questID) {
+                return q;
+            }
+        }
+        return null;
+    }
 
-	public Quest getQuest(String questName) {
-		for (Quest q : quests) {
-			if (q.getQuestName().equals(questName)) {
-				return q;
-			}
-		}
-		return null;
+    public Quest getQuest(String questName) {
+        for (Quest q : quests) {
+            if (q.getQuestName().equals(questName)) {
+                return q;
+            }
+        }
+        return null;
 
-	}
+    }
 
-	public Quest registerNewQuest(String questName, int bandNummer) {
-		Quest q = new Quest(questName, bandNummer);
-		quests.add(q);
-		q.registerQuestIfnotExist();
-		return q;
-	}
+    public Quest registerNewQuest(String questName, int bandNummer) {
+        Quest q = new Quest(questName, bandNummer);
+        quests.add(q);
+        q.registerQuestIfnotExist();
+        return q;
+    }
 
+    public ArrayList<Quest> getQuests() {
+        return quests;
+    }
 
 }
