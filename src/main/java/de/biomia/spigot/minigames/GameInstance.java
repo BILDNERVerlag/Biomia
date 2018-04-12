5475af1cda34459f7f275b6be4fecf01944e0ebf
DerJulsn <julian.bildner@web.de>
2018-04-12 15:45:43 +0200

    tried to fix gamerule bug

--- src/main/java/de/biomia/spigot/minigames/GameInstance.java
+++ src/main/java/de/biomia/spigot/minigames/GameInstance.java
@@ -35,6 +35,7 @@ public class GameInstance {
         this.mapDisplayName = mapDisplayName;
         this.world = new WorldCreator(mapName).createWorld();
         world.setGameRuleValue("announceAdvancements", "false");
+        world.setGameRuleValue("keepInventory", "false");
         MinigamesConfig.mapName = mapDisplayName;
 
         switch (type) {
