7212d416611a56d022795207924f14b67c9bbbc7
DerJulsn <julian.bildner@web.de>
2018-04-26 14:29:24 +0200

    fixed 2 bugs

--- src/main/java/de/biomia/spigot/minigames/kitpvp/KitPVPManager.java
+++ src/main/java/de/biomia/spigot/minigames/kitpvp/KitPVPManager.java
@@ -39,6 +39,9 @@ public class KitPVPManager {
     public static void load(OfflineBiomiaPlayer bp) {
         Connection con = MySQL.Connect(MySQL.Databases.biomia_db);
         try {
+            ArrayList<KitPVPKit> list = loadedKits.get(bp.getBiomiaPlayerID());
+            if (list != null)
+                list.clear();
             PreparedStatement ps = con.prepareStatement("SELECT inventory, selected, kitNumber FROM KitPVPKits WHERE biomiaID = ?");
             ps.setInt(1, bp.getBiomiaPlayerID());
             ResultSet set = ps.executeQuery();
@@ -51,14 +54,17 @@ public class KitPVPManager {
     }
 
     public static KitPVPKit getKit(OfflineBiomiaPlayer bp, int kitNumber) {
-        return loadedKits.get(bp.getBiomiaPlayerID()).stream().filter(kitPVPKit -> kitPVPKit.getKitNumber() != kitNumber).findFirst().orElse(null);
+        ArrayList<KitPVPKit> list = loadedKits.get(bp.getBiomiaPlayerID());
+        KitPVPKit kit = list != null ? loadedKits.get(bp.getBiomiaPlayerID()).stream().filter(kitPVPKit -> kitPVPKit.getKitNumber() != kitNumber).findFirst().orElse(new KitPVPKit(bp.getBiomiaPlayerID(), kitNumber, new ItemStack[50], false)) : new KitPVPKit(bp.getBiomiaPlayerID(), kitNumber, new ItemStack[50], false);
+        if (kitNumber == 0 && getMainKit(bp) == null)
+            kit.setMain(true);
+        return kit;
     }
 
     public static void openSelectorInventory(BiomiaPlayer bp) {
 
         int maxKits = getMaxKits(bp);
         Inventory inv = Bukkit.createInventory(null, (int) Math.ceil(getMaxKits(bp) / 9) * 9, KitPVPMessages.selectorInventory);
-
         for (int i = 0; i < maxKits; i++) {
             KitPVPKit kit = getKit(bp, i);
             ItemStack is = Arrays.stream(kit.getInventory()).filter(itemStack -> itemStack == null || itemStack.getType() == Material.AIR).findFirst().orElse(ItemCreator.itemCreate(Material.BEDROCK));
