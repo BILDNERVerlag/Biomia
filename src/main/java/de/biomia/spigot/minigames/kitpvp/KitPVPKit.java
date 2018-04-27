eac8e8199d8528398651810bbfa20b594363f237
DerJulsn <julian.bildner@web.de>
2018-04-27 15:51:22 +0200

    fixed syntax error

--- src/main/java/de/biomia/spigot/minigames/kitpvp/KitPVPKit.java
+++ src/main/java/de/biomia/spigot/minigames/kitpvp/KitPVPKit.java
@@ -35,7 +35,7 @@ public class KitPVPKit {
     }
 
     public void save() {
-        if (MySQL.executeQuerygetint("Select KitPVPKits WHERE kitNumber = " + kitNumber + " AND `biomiaID` = " + biomiaID, "biomiaID", MySQL.Databases.biomia_db) != -1)
+        if (MySQL.executeQuerygetint("Select biomiaID from KitPVPKits WHERE kitNumber = " + kitNumber + " AND `biomiaID` = " + biomiaID, "biomiaID", MySQL.Databases.biomia_db) != -1)
             MySQL.executeUpdate("UPDATE `KitPVPKits` SET `inventory`= " + Base64.toBase64(inventory) + " WHERE kitNumber = " + kitNumber + " AND `biomiaID` = " + biomiaID, MySQL.Databases.biomia_db);
         else {
             MySQL.executeUpdate("INSERT INTO KitPVPKits (`biomiaID`, `kitNumber`, `inventory`, `selected`) VALUES (" + biomiaID + "," + kitNumber + ",'" + Base64.toBase64(inventory) + "'," + (kitNumber == 0) + ")", MySQL.Databases.biomia_db);
