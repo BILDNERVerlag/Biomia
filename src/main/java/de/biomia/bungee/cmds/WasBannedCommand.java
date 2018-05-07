6da01404a1f3d3029c1aaf718b7af8fa5e2e92e0
DerJulsn <julian.bildner@web.de>
2018-05-07 15:01:40 +0200

    replaced a few hasPermission() methods with isStaff()...

--- src/main/java/de/biomia/bungee/cmds/WasBannedCommand.java
+++ src/main/java/de/biomia/bungee/cmds/WasBannedCommand.java
@@ -2,10 +2,13 @@ package de.biomia.bungee.cmds;
 
 import de.biomia.bungee.BungeeBiomia;
 import de.biomia.bungee.BungeeMain;
+import de.biomia.spigot.Biomia;
+import de.biomia.spigot.BiomiaPlayer;
 import de.biomia.universal.Messages;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Command;
+import org.bukkit.entity.Player;
 
 public class WasBannedCommand extends Command {
 
@@ -16,20 +19,20 @@ public class WasBannedCommand extends Command {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        if (sender.hasPermission("biomia.*") || sender.hasPermission("biomia.wasbanned")) {
-
-            if (args.length >= 1) {
-                String name = args[0];
-                int i;
-                int id = BungeeBiomia.getOfflineBiomiaPlayer(name).getBiomiaPlayerID();
-                i = (int) BungeeMain.cachedBans.stream().filter(ban -> ban.getBiomiaID() == id).count();
-                if (i > 0)
-                    sender.sendMessage(new TextComponent(String.format("§bDer Spieler §c%s wurde bereits §c%dx §bgebannt!", name, i)));
-                else
-                    sender.sendMessage(new TextComponent(String.format("§bDer Spieler §c%s §bwurde noch §cnie §bgebannt!", name)));
-            } else
-                sender.sendMessage(new TextComponent("\u00A7cBitte nutze \u00A7b/wasbanned <Spieler>"));
-        } else
+        BiomiaPlayer bp = Biomia.getBiomiaPlayer((Player) sender);
+        if (!bp.isSrStaff()) {
             sender.sendMessage(new TextComponent(Messages.NO_PERM));
+            return;
+        }
+        if (args.length >= 1) {
+            String name = args[0];
+            int id = BungeeBiomia.getOfflineBiomiaPlayer(name).getBiomiaPlayerID();
+            int i = (int) BungeeMain.cachedBans.stream().filter(ban -> ban.getBiomiaID() == id).count();
+            if (i > 0)
+                sender.sendMessage(new TextComponent(String.format("%sDer Spieler %s%s%s wurde bereits %s%dx%s gebannt!", Messages.COLOR_MAIN, Messages.COLOR_SUB, name, Messages.COLOR_MAIN, Messages.COLOR_SUB, i, Messages.COLOR_MAIN)));
+            else
+                sender.sendMessage(new TextComponent(String.format("%sDer Spieler %s%s%s wurde noch %snie %sgebannt!", Messages.COLOR_MAIN, Messages.COLOR_SUB, name, Messages.COLOR_MAIN, Messages.COLOR_SUB, Messages.COLOR_MAIN)));
+        } else
+            sender.sendMessage(new TextComponent(String.format("%sBitte nutze %s/wasbanned <Spieler>", Messages.COLOR_MAIN, Messages.COLOR_SUB)));
     }
 }
