3696f03f260e0c9697452ffc84bb751729921d85
DerJulsn <julian.bildner@web.de>
2018-05-11 21:36:14 +0200

    developed on ParrotCanon

--- src/main/java/de/biomia/spigot/minigames/parrot/ParrotCanon.java
+++ src/main/java/de/biomia/spigot/minigames/parrot/ParrotCanon.java
@@ -1,27 +1,45 @@
 package de.biomia.spigot.minigames.parrot;
 
+import com.boydti.fawe.FaweAPI;
+import com.sk89q.worldedit.bukkit.BukkitUtil;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
+import com.sk89q.worldedit.math.transform.AffineTransform;
+import de.biomia.spigot.minigames.GameTeam;
+import de.biomia.spigot.minigames.TeamColor;
+
+import java.io.File;
+import java.io.IOException;
+
 public class ParrotCanon {
 
-    //TODO
+    private final CanonType type;
+    private int level = 0;
+    private final GameTeam team;
+    private final ParrotCanonPoint canonPoint;
 
-//    private final CanonType type;
-//    private int level = 0;
-//    private int maxLevel;
-//    private HashMap<Integer, ItemStack> levelStages;
-//
-//    public ParrotCanon(CanonType type, HashMap<Integer, ItemStack[][][]> map) {
-//        maxLevel = map.size() - 1;
-//        this.type = type;
-//    }
-//
-//    public void spawn(Location loc) {
-//
-//        //TODO place canon at location loc
-//
-//    }
-//
-//    enum CanonType {
-//        SHOTGUN, BAZOOKA, SNIPER, GUN4, GUN5
-//    }
+    public ParrotCanon(CanonType type, GameTeam team, ParrotCanonPoint canonPoint) {
+        this.team = team;
+        this.canonPoint = canonPoint;
+        this.type = type;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+        spawn();
+    }
+
+    public void spawn() {
+        try {
+            ClipboardFormat.SCHEMATIC.load(new File(type.name() + "_" + level)).paste(FaweAPI.getWorld(team.getMode().getInstance().getWorld().getName()), BukkitUtil.toVector(canonPoint.getLocation()), false, false, new AffineTransform().rotateY(team.getColor() == TeamColor.RED ? 90 : -90));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public enum CanonType {
+        SHOTGUN, BAZOOKA, SNIPER, CANON, AK_HALBAUTOMATIK
+    }
+
+
 
 }
---
+++ src/main/java/de/biomia/spigot/minigames/parrot/ParrotCanonPoint.java
@@ -0,0 +1,14 @@
+package de.biomia.spigot.minigames.parrot;
+
+import org.bukkit.Location;
+
+public class ParrotCanonPoint {
+
+    //TODO
+
+    private Location location = null;
+
+    public Location getLocation() {
+        return location;
+    }
+}
