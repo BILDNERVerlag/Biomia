3c69c8ec0c59f01733805970399f2eec8acb69a9
aer0des1gn <kuebler.aaron@gmx.de>
2018-12-10 09:25:09 +0100

    fixed pom so that project works in intellij idea18

--- src/main/java/de/biomia/spigot/tools/VoidWorldGenerator.java
+++ src/main/java/de/biomia/spigot/tools/VoidWorldGenerator.java
@@ -9,15 +9,17 @@ import java.util.Random;
 
 public class VoidWorldGenerator extends ChunkGenerator {
 
-    public final ChunkGenerator.ChunkData generateChunkData(World world, Random random, int x, int z, ChunkGenerator.BiomeGrid biome) {
-        ChunkGenerator.ChunkData chunkData = this.createChunkData(world);
-        for (int i = 0; i < 16; ++i) {
-            for (int j = 0; j < 16; ++j) {
-                biome.setBiome(i, j, Biome.VOID);
-            }
-        }
-        return chunkData;
-    }
+    //FIXME: ChunkData no longer exists (?)
+
+//    public final ChunkGenerator.ChunkData generateChunkData(World world, Random random, int x, int z, ChunkGenerator.BiomeGrid biome) {
+//        ChunkGenerator.ChunkData chunkData = this.createChunkData(world);
+//        for (int i = 0; i < 16; ++i) {
+//            for (int j = 0; j < 16; ++j) {
+//                biome.setBiome(i, j, Biome.VOID);
+//            }
+//        }
+//        return chunkData;
+//    }
 
     public final Location getFixedSpawnLocation(World world, Random random) {
         return new Location(world, 0, 65, 0);
